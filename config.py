74e7a1d9ad3e8da61e790177e15f12cf0da336ae
Brian Sunbury <bsunbury29@gmail.com>
2019-03-22 21:29:14 -0700

    root files

---
+++ config.py
@@ -0,0 +1,26 @@
+"""
+This file contains all of the configuration values for the application.
+Update this file with the values for your specific Google Cloud project.
+You can create and manage projects at https://console.developers.google.com
+"""
+
+import os
+
+# The secret key is used by Flask to encrypt session cookies.
+SECRET_KEY = 'secret'
+
+# There are three different ways to store the data in the application.
+# You can choose 'datastore', 'cloudsql', or 'mongodb'. Be sure to
+# configure the respective settings for the one you choose below.
+# You do not have to configure the other data backends. If unsure, choose
+# 'datastore' as it does not require any additional configuration.
+DATA_BACKEND = 'mongodb'
+
+# Google Cloud Project ID. This can be found on the 'Overview' page at
+# https://console.developers.google.com
+PROJECT_ID = 'your-project-id'
+
+# Mongo configuration
+# If using mongolab, the connection URI is available from the mongolab control
+# panel. If self-hosting on compute engine, replace the values below.
+MONGO_URI = 'mongodb://user:password@host:27017/database'
